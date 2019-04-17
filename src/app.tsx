934ec247c7fd85703506d32ab248eec1dcc43d28
Evan Sherwood <evan@sherwood.io>
2019-04-16 23:57:17 -0700

    add emotion

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,12 @@
+/** @jsx jsx */
+
+import { css, jsx } from "@emotion/core";
 import * as React from "react";
 
-export const App: React.FunctionComponent = () => <h1>Hello World!</h1>;
+const style = css`
+  font-family: Montserrat, Ubuntu, sans-serif;
+`;
+
+export const App: React.FunctionComponent = () => (
+  <h1 css={style}>Hello World!</h1>
+);
